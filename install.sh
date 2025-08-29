a5ecbdf9f55c3abd3f3c86c0d19b8fdecf00b5f5
Alexandre Synnott <2467035@cstjean.qc.ca>
2025-08-29 11:14:20 -0400

    install fini

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 mkdir ~/bin
+mkdir ~/cegep
+mkdir ~/projets
+
+
+sudo pacman -S git
+sudo pacman -S vim
+sudo pacman -S gcc
+sudo pacman -S gdb
+sudo pacman -S python
+sudo pacman -S python-pip
+sudo pacman -S python-venv
